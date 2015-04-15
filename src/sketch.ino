52c0afde7b7f60e45bf61503720b68463b8b7e5c
Nolan Darilek <nolan@thewordnerd.info>
2015-04-15 02:00:03 -0500

    Remove CHIRP table and generate all chirps in native code.

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,31 +1,48 @@
 #include <MozziGuts.h> // this makes everything work 
 #include <Metronome.h>
-#include <SampleHuffman.h>
+#include <Oscil.h>
+#include <tables/sin2048_int8.h>
 
-#include "chirp.h"
-
-SampleHuffman chirp(CHIRP_SOUNDDATA, CHIRP_HUFFMAN, CHIRP_SAMPLE_BITS);
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> sinWave(SIN2048_DATA);
 
 Metronome metronome(500);
 
-#define DEBUG 1
+#define DEBUG true
+
+int baseFreq = 8000;
 
 void setup() {
   startMozzi();
+  sinWave.setFreq(baseFreq);
   if(DEBUG)
     Serial.begin(9600);
 }
 
+unsigned long lastReady = 0;
+
+const int bandwidth = 3000;
+
 void updateControl(){
+  unsigned long now = mozziMicros();
   if(metronome.ready()) {
+    lastReady = now;
+    sinWave.setFreq(baseFreq);
+    sinWave.setPhase(0);
+  } else if(now-lastReady <= 10000) {
+    float ratio = (now-lastReady)/10000;
     if(DEBUG)
-      Serial.println("chirp");
-    chirp.start();
+      Serial.println(ratio);
+    sinWave.setFreq(baseFreq+((int)ratio*bandwidth));
+  } else {
+    lastReady = 0;
   }
 }
 
 int updateAudio(){
-  return chirp.next();
+  if(lastReady == 0)
+    return 0;
+  else
+    return sinWave.next();
 }
 
 void loop() {
